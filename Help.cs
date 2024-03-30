094204c49a25cfe6e177ba5c4cdcca0985d4152e
Aldair <aldair.zamora@outlook.com>
2024-03-30 14:37:11 -0600

    initial commit

---
+++ Help.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleTodoApp;
+
+public class Help
+{
+    // Empty constructor
+    public Help() { }
+
+
+    // Display menu
+    public void DisplayMenu()
+    {
+        Console.WriteLine("\n******** Commands **********");
+        Console.WriteLine("add: Adds a new list to the todo.");
+        Console.WriteLine("list: Shows all the todo's.");
+
+        Console.WriteLine();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using SimpleTodoApp;
+
+Console.WriteLine("Todo app, to get the instructions pass the command 'help'");
+string? input = "";
+
+do
+{
+    // Ask user to provide a command
+    Console.Write("Enter command: ");
+    input = Console.ReadLine();
+
+    switch (input!.ToLower())
+    {
+        case "help":
+            Help help = new Help();
+            help.DisplayMenu();
+            break;
+        default:
+            Console.WriteLine("Incorrect argument. Pass help if you need assistance.");
+            break;
+    }
+
+} while (input != "exit");
+
+Console.WriteLine("Good Bye!");
+
+Console.Read();
